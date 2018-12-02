d89abb6ae7b4e3d6f1f59fff42944d4bf2ba2ef9
chauncey <qingqi_lei@icloud.com>
2018-12-02 22:54:21 +0800

    Initial commit

---
+++ LearningtoProgramWithPython3/TuplesStringsLoops.py
@@ -0,0 +1,7 @@
+programming_languages = "Python","Java","C++","C#" # immutable,
+programming_languages1 = ("Python","Java","C++","C#") # it's the same with the first line
+print(type(programming_languages))
+print(type(programming_languages1))
+
+for language in programming_languages1:
+    print(language)
