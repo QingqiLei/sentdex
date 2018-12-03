da241e04ce45eb128b1f92fdc9022cb29850e24e
chauncey <qingqi_lei@icloud.com>
2018-12-03 23:00:56 +0800

    index of a list []

---
+++ LearningtoProgramWithPython3/IndexesSlices.py
@@ -0,0 +1,14 @@
+l = [1, 2, 3, 4, 5]  # indexing from 0, it's a list
+print(l[1])
+
+print(l[-1])  # the last one
+
+print(l[1:3])  # from index 1 to index 2
+l[1] = 99  # change number in a list
+print(l)
+
+game = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+game[0][1] = 1
+game[0][0] = "dsfewf"
+print(game)
+
